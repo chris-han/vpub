7fa41a7dbe8a9534cd0aabf2c44be72cd448f2e3
Chris Han <chrishan2000@hotmail.com>
2023-06-09 20:37:12 +0800

    add ssr

--- app.py
+++ app.py
@@ -47,6 +47,36 @@ def base64_encode():
     else:
         return "it's fresh here"
 
+@app.route("/ssr", methods=['GET'])  
+def get_ssr():
+
+    """     
+    if request.method == 'POST':
+        text = request.form['text']
+        encoded_text = base64.b64encode(text.encode('utf-8'))
+        return encoded_text.decode('utf-8')
+    else:
+        return '''
+            <form method="POST">
+                Text: <input name="text">
+                <input type="submit">
+            </form>
+        ''' 
+    """
+    temp_file_path = os.path.join(tempfile.gettempdir(), 'v2ray_config.json')
+    if os.path.exists(temp_file_path):
+        with open(temp_file_path, 'rb') as f:
+            b64_json = f.read()
+        
+        json_obj = json.loads(base64.b64decode(b64_json).decode())
+        json_str = json.dumps(json_obj)
+
+        #text = '{ "v": "2", "ps": "JPE2", "add": "52.140.199.214", "port": "47330", "id": "b0f229a6-f383-40f0-b467-79ff45973320", "aid": "0", "scy": "auto", "net": "tcp", "type": "none", "host": "", "path": "", "tls": "", "sni": "", "alpn": "", "fp": "" }'
+        encoded_text =  base64.b64encode(json_str.encode('utf-8'))    
+        return 'ssr://'+ encoded_text.decode('utf-8')
+    else:
+        return "it's fresh here"
+    
 @app.route("/updateip", methods=['GET'])
 def updateip():
     if request.method == 'GET':
